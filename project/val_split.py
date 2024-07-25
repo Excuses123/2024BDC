9510932a9d8cc95a9091d4c4421eba84326c0a90
lujie <lujie@4399inc.com>
2024-07-25 11:09:12 +0800

    复赛调整

--- project/val_split.py
+++ project/val_split.py
@@ -8,7 +8,7 @@ def cmd_args():
     parser = argparse.ArgumentParser(description="Model Args")
     parser.add_argument("--seed", type=int, default=1024, help="random seed.")
     parser.add_argument('--val_rate', type=float, default=0.25, help='验证比例')
-    parser.add_argument("--num_sta", type=int, default=300, help="验证的station个数")
+    parser.add_argument("--num_sta", type=int, default=240, help="验证的station个数")
     parser.add_argument("--data_path", type=str, default='./eval_data')
 
     return parser.parse_args()
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     win = np.array([random.randint(0, total_time - n * 3 - 168 - 72) for _ in range(71)])
     np.save(os.path.join(args.data_path, 'sample_win.npy'), win)
     # 随机240个station进行验证
-    sta = np.array([random.randint(0, num_station) for _ in range(240)])
+    sta = np.array([random.randint(0, num_station) for _ in range(args.num_sta)])
     np.save(os.path.join(args.data_path, 'sample_station.npy'), sta)
 
     a_train = a[:n]
@@ -46,9 +46,9 @@ if __name__ == '__main__':
     b_val = b[n*3:]     # [4386, 3850, 1]
     c_val = c[n*3:]     # [4386, 3850, 1]
 
-    a_val = np.concatenate([a_val[None, i//3: i//3+56+24, :, :, sta] for i in win], axis=0)  # [71, 56+24, 4, 9, 240]
-    b_val = np.concatenate([b_val[None, i: i+168+72, sta, :] for i in win], axis=0)  # [71, 168+72, 240, 1]
-    c_val = np.concatenate([c_val[None, i: i+168+72, sta, :] for i in win], axis=0)  # [71, 168+72, 240, 1]
+    a_val = np.concatenate([a_val[None, i//3: i//3+56+24, :, :, sta] for i in win], axis=0)  # [71, 56+24, 4, 9, S]
+    b_val = np.concatenate([b_val[None, i: i+168+72, sta, :] for i in win], axis=0)  # [71, 168+72, S, 1]
+    c_val = np.concatenate([c_val[None, i: i+168+72, sta, :] for i in win], axis=0)  # [71, 168+72, S, 1]
 
     print(a_train.shape)
     print(b_train.shape)
